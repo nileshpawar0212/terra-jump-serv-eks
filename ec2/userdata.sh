34743328a997d90680f6b01605654b6d3e08bfdb
nileshpawar0212 <nileshpawar0212@gmail.com>
2025-09-24 05:22:59 +0000

    test1

--- ec2/userdata.sh
+++ ec2/userdata.sh
@@ -52,10 +52,10 @@ usermod -aG docker ec2-user || true
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-sudo dnf upgrade
+sudo dnf upgrade -y
 # Add required dependencies for the jenkins package
-sudo dnf install fontconfig java-17-amazon-corretto-devel
-sudo dnf install jenkins
+sudo dnf install fontconfig java-17-amazon-corretto-devel -y
+sudo dnf install jenkins -y
 sudo systemctl daemon-reload
 systemctl enable jenkins
 systemctl start jenkins || true
